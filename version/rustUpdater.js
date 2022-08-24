4b3806127054431ae6b4fb1fa20a8d8243803c77
Galitan-dev <galitan.dev@gmail.com>
2022-08-24 16:31:43 +0200

    ci: dump Cargo.lock version

--- version/rustUpdater.js
+++ version/rustUpdater.js
@@ -4,14 +4,16 @@
  */
 module.exports.readVersion = function (contents) {
   const lines = contents.split("\n");
-  const lineIndex = lines.findIndex((line) => line.startsWith("version"));
+  const lineIndex =
+    lines.findIndex((line) => line.match(/^name\s?=\s?["']typino["']$/)) + 1;
   const quotedVersion = lines[lineIndex].split(/\s?=\s?/)[1];
   return quotedVersion.substring(1, quotedVersion.length - 1);
 };
 
 module.exports.writeVersion = function (contents, version) {
   const lines = contents.split("\n");
-  const lineIndex = lines.findIndex((line) => line.startsWith("version"));
+  const lineIndex = // Line under `name = "typino"`
+    lines.findIndex((line) => line.match(/^name\s?=\s?["']typino["']$/)) + 1;
   lines[lineIndex] = `version = "${version}"`;
   return lines.join("\n");
 };
